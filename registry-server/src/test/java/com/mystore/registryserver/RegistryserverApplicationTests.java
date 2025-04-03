c468ede814b0ec88339b82daaff73d1b76f08b5d
yrajm1997 <yograj.m@talentsprint.com>
2025-04-03 23:44:04 +0530

    other services servers added

---
+++ registry-server/src/test/java/com/mystore/registryserver/RegistryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystore.registryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
